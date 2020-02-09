3a62bba1f1915b57af5ed93b0976e029d9eff64f
Manuela Redinciuc <manutzu15@gmail.com>
2020-02-09 09:40:13 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,19 +14,18 @@ WIN_COMBINATIONS = [
 [1,4,7],
 [2,5,8],
 [0,4,8],
-[2,4,6]
+[6,4,2]
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-
-    position_1 = board[win_index_1] # value of board at win_index_1
-    position_2 = board[win_index_2] # value of board at win_index_2
-    position_3 = board[win_index_3] # value of board at win_index_3
-    position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+ WIN_COMBINATIONS.each do |win_combination|
+   win_index_1 = win_combination[0]
+   win_index_2 = win_combination[1]
+   win_index_3 = win_combination[2]
+   position_1 = board[win_index_1] # value of board at win_index_1
+   position_2 = board[win_index_2] # value of board at win_index_2
+   position_3 = board[win_index_3] # value of board at win_index_3
+   position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
  end
 end
 
@@ -34,10 +33,3 @@ end
 
 
 
-#def won?(board)
-#  WIN_COMBINATIONS.detect do |combo|
-#    board[combo[0]] == board[combo[1]] &&
-#    board[combo[1]] == board[combo[2]] &&
-#    position_taken?(board, combo[0])
-#  end
-#end
