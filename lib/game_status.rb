405559ad0acf954a35141aa552244bb7f0a98071
Manuela Redinciuc <manutzu15@gmail.com>
2020-02-09 10:45:01 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -57,10 +57,7 @@ end
 
 
 def winner(board)
- if won?(board) == "X"
-   return "X"
- elsif won?(board) == "O"
-   return "O"
- else false
- end
+   if won?(board)
+      return board[won?(board)[0]]
+   end
 end
