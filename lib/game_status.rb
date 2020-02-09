15cf8ac6de4069a119f889d8b3d1cb31f89b6e4b
Manuela Redinciuc <manutzu15@gmail.com>
2020-02-09 09:45:35 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,7 +29,7 @@ def won?(board)
   if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
     return win_combination
   else
-    return false
+    false
   end
  end
 end
