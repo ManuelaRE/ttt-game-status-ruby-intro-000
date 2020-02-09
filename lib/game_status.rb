e7c4f34ff71771290a6fa8c4c80eda03c2ccdf87
Manuela Redinciuc <manutzu15@gmail.com>
2020-02-09 09:13:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,37 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+WIN_COMBINATIONS = [
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[2,4,6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.select do |wincomb|
+      if
+        board[wincomb[0]] == board[wincomb[1]] &&
+        board[wincomb[1]] == board[wincomb[2]] &&
+        position_taken?(board, wincomb[0])
+      return wincomb
+      else false
+    end
+  end
+end
+
+
+
+#def won?(board)
+#  WIN_COMBINATIONS.detect do |combo|
+#    board[combo[0]] == board[combo[1]] &&
+#    board[combo[1]] == board[combo[2]] &&
+#    position_taken?(board, combo[0])
+#  end
+#end
